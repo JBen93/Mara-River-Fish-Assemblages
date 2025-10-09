8d78737314dccac495b370ff22ad026727c66468
JBen93 <joshua.benjamin@ufl.edu>
2025-10-09 13:55:47 -0400

    added the data script

--- R-scripts/1. Labeo victorianus distribution.R
+++ R-scripts/1. Labeo victorianus distribution.R
@@ -1,4 +1,4 @@
-#Fish assemblages in the Mara River
+#Labeo victorianus distribution analysis in the Mara River
 # Load necessary libraries
 library(tidyverse)
 library(readr)
@@ -8,9 +8,11 @@ library(ggplot2)
 library(car)  # For Levene's test
 library(rstatix)  # For normality and variance tests
 
+#datasource
+
 # Read data from Google Sheets
-watermetals <- readr::read_csv(
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSFJjla0jcvd2MqIFU0oPlIMGqLM5QQJnN0C9ognk6WRrqxtYnZgCRZ-14J2S1mtl4NTUqb7y16bWYm/pub?gid=1141386352&single=true&output=csv",
+currentfish<- readr::read_csv(
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRDo5laGSxF444O2xpHBPq4papf5IJd5VQ6BOFoUKGZIZZRqAp5gHsWrWfv-P3A2OBeJUH16Gn4N_ng/pub?gid=152464398&single=true&output=csv",
   show_col_types = FALSE
 ) %>%
   filter(Location_ID %in% paste0("M", 1:9), sampling_year %in% 2021:2023)
