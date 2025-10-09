725fcd36f8645e9c5113122af29b4e61ee233336
JBen93 <joshua.benjamin@ufl.edu>
2025-10-09 05:22:07 -0400

    made changes

---
+++ Setting R environmet.R
@@ -0,0 +1,11 @@
+install.packages("renv")
+renv::init()
+install.packages("tidyverse")
+install.packages("psych")
+install.packages("vegan")
+install.packages("terra")
+install.packages("raster")
+
+renv::snapshot()
+renv::restore()
+summary(tidyverse)
